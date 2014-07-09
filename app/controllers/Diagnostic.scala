afb33bc30954d3e8552bb4d0f3fb840c532b8208
dujuanxian <juan.du@rea-group.com>
2014-07-09 17:49:30 +0800

    Xinyu & Juan :diagnostic ping

--- app/controllers/Diagnostic.scala
+++ app/controllers/Diagnostic.scala
@@ -2,12 +2,13 @@ package controllers
 
 import play.api.mvc._
 
-/**
- * Created by yongmin_xia on 9/07/2014.
- */
 object Diagnostic extends Controller {
   def name = Action {
     Ok("User Manager Api")
+  }
+
+  def ping = Action {
+    Ok("")
   }
 
 }
--- test/controllers/DiagnosticSpec.scala
+++ test/controllers/DiagnosticSpec.scala
@@ -6,9 +6,6 @@ import org.specs2.runner._
 import play.api.test._
 import play.api.test.Helpers._
 
-/**
- * Created by yongmin_xia on 9/07/2014.
- */
 @RunWith(classOf[JUnitRunner])
 class DiagnosticSpec  extends Specification {
   "Diagnostic" should {
@@ -17,6 +14,12 @@ class DiagnosticSpec  extends Specification {
       status(response) must equalTo(OK)
       contentType(response) must beSome.which(_ == "text/plain")
       contentAsString(response) must contain ("User Manager Api")
+    }
+
+    "return OK when ping" in new WithApplication{
+      val ping = route(FakeRequest(GET, "/diagnostic/ping")).get
+
+      status(ping) must equalTo(OK)
     }
   }
 
